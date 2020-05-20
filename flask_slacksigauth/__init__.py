d1a14481ed40fdd3dbf8520306672c58da1d22f3
Emilio A. Escobar <eescobar@gmail.com>
2020-05-20 15:20:47 -0700

    Raise attribute error if no signing secret is found

--- flask_slacksigauth/__init__.py
+++ flask_slacksigauth/__init__.py
@@ -40,6 +40,9 @@ def slack_sig_auth(f):
 
         if slack_signing_secret is None:
             slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
+           
+        if slack_signing_secret is None:
+            raise AttributeError('No slack signing secret configured, please set as environment variable or application configuration')
 
         req_timestamp = request.headers.get('X-Slack-Request-Timestamp')
         req_signature = request.headers.get('X-Slack-Signature')
